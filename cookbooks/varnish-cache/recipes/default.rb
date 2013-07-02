961b42945a215b3f2e51a7aacdca8d08ce595aec
Валентин Настенко <versus.ua@gmail.com>
2013-07-02 15:36:12 +0300

    add varnish-cache to install

--- cookbooks/varnish-cache/recipes/default.rb
+++ cookbooks/varnish-cache/recipes/default.rb
@@ -15,7 +15,6 @@ execute "install varnish yum repo" do
   command "rpm --nosignature -i http://repo.varnish-cache.org/redhat/varnish-3.0/el5/noarch/varnish-release-3.0-1.noarch.rpm"
   creates "/etc/yum.repos.d/varnish.repo"
   action :run
-  notifies :run, "execute[yum_update", :immediately
 end
 
 execute "yum_update" do
