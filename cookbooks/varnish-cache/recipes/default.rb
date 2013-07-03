aba45800cf5582eca14a082570569a72b4e841d1
Валентин Настенко <versus.ua@gmail.com>
2013-07-03 14:47:19 +0300

    add init.d to apache

--- cookbooks/varnish-cache/recipes/default.rb
+++ cookbooks/varnish-cache/recipes/default.rb
@@ -9,19 +9,6 @@
 
 
 
-#rpm --nosignature -i http://repo.varnish-cache.org/redhat/varnish-3.0/el5/noarch/varnish-release-3.0-1.noarch.rpm
-
-execute "install varnish yum repo" do
-  command "rpm --nosignature -i http://repo.varnish-cache.org/redhat/varnish-3.0/el5/noarch/varnish-release-3.0-1.noarch.rpm"
-  creates "/etc/yum.repos.d/varnish.repo"
-  action :run
-end
-
-execute "yum_update" do
-  command "yum update"
-  action :nothing
-end
-
 package "varnish" do
   action :install
   not_if "rpm -q varnish"
