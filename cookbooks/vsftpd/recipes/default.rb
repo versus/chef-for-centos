63758d0be8d614395bffe48943675a258cba974f
Валентин Настенко <versus.ua@gmail.com>
2013-07-03 16:16:17 +0300

    add vsftpd

--- cookbooks/vsftpd/recipes/default.rb
+++ cookbooks/vsftpd/recipes/default.rb
@@ -2,7 +2,13 @@
 # Cookbook Name:: vsftpd
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
+# Copyright (C) 2013 Valentyn Nastenko
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+package "vsftpd" do
+  action :install
+  not_if "rpm -q vsftpd"
+end
+
